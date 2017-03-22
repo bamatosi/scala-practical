b37c0d47632600c7e648d86b5f3c5b08a5aaad9a
Bartosz Matosiuk <bartosz.matosiuk@gmail.com>
2017-03-22 13:41:12 +0100

    Reorganize model for tweets better

--- app/model/Tweet.scala
+++ app/model/Tweet.scala
@@ -1,9 +1,20 @@
 package model
 
 import play.api.libs.json._
+import slick.driver.H2Driver.api._
 
-// Case class providing model for a tweet
+// Case class providing application model for a tweet
 case class Tweet (id: Option[Long] = None, author: String, message: String, link: String)
+
+// Table class priding twwet model for Slick
+class TweetsTable(tag: Tag) extends Table[Tweet](tag, "TWEET") {
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def author = column[String]("author")
+  def message = column[String]("message")
+  def link = column[String]("link")
+
+  override def * = (id.?, author, message, link) <>(Tweet.tupled, Tweet.unapply)
+}
 
 // Tweet format needed for JSON serializer
 object TweetJSON {
--- app/repos/TweetsRepo.scala
+++ app/repos/TweetsRepo.scala
@@ -3,7 +3,7 @@ package repos
 import slick.driver.H2Driver.api._
 import play.api.libs.json.{JsObject, Json}
 import concurrent._
-import model.Tweet
+import model.{Tweet, TweetsTable}
 import scala.util.{Success, Failure}
 import concurrent.ExecutionContext.Implicits.global
 
@@ -19,7 +19,7 @@ trait TweetsRepo {
 class TweetsRepoImpl extends TweetsRepo {
 
   /* initialize repository */
-  val tweets = TableQuery[Tweets]
+  val tweets = TableQuery[TweetsTable]
   val db = Database.forConfig("h2mem")
   val setupAction: DBIO[Unit] = DBIO.seq(
     tweets.schema.create
@@ -33,7 +33,10 @@ class TweetsRepoImpl extends TweetsRepo {
 
   /* Public API */
   def findById(id: Long): Future[Tweet] = db.run(filterById(id).result.head)
-  def list: Future[Seq[Tweet]] = db.run(tweets.result)
+  def list: Future[Seq[Tweet]] = {
+    insert(new Tweet(Option(1.asInstanceOf[Long]), "a", "b", "c"))
+    db.run(tweets.result)
+  }
   def insert(tweet: Tweet): Future[Int] = db.run(tweets += tweet)
   def update(id: Long, tweet: Tweet): Future[Int] = db.run(filterById(id).update(tweet))
   def delete(id: Long): Future[Int] = db.run(filterById(id).delete)
